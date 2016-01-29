12f571993954b8c5bece8f5830a213821ddb0a93
Dolf856 <dfindley856@me.com>
2016-01-28 19:53:13 -0600

    traverse

---
+++ public/essential_methods.js
@@ -0,0 +1,13 @@
+$("#worthwhile-button").click(function){
+
+//get the input of the first text box and set to variable
+var elementInput = $("element-input").val();
+
+//get the input of the second box and set it to variable
+var
+
+
+
+
+
+});
